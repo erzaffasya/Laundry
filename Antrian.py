a94c5fc2073a0c2380ac7a84395d46a27960940f
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2019-12-14 18:23:19 +0800

    master

--- Antrian.py
+++ Antrian.py
@@ -1,15 +1,16 @@
-from typing import List, Any
-
-
 class Queue:
     def __init__(self):
         self.items = []
+
     def isEmpty(self):
         return self.items == []
+
     def enqueue(self, item):
         self.items.insert(0, item)
+
     def dequeue(self):
         return self.items.pop()
+
     def rear(self):
         return self.items[0]
 
@@ -21,6 +22,18 @@ class Queue:
 
     def open(self):
         return self.items
-
-    def getdata(self, i, j):
+    
+    def getdata(self,i,j):
         return self.items[i][j]
+    
+    def getkolom(self,j):
+        kolom=[]
+        for i in self.items[:][:]:
+            kolom.append(i[j])
+        return kolom
+
+    def getbaris(self,i):
+        baris=[]
+        for j in self.items[:][:]:
+            kolom.append(j[i])
+        return kolom
